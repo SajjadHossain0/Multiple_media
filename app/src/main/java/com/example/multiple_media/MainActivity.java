50e9c55a1337ea27b9f1d0ed087fe7a7599a7a40
Clock <mdsajjadhossain614@gmail.com>
2023-05-20 00:54:57 +0600

    Multiple media.

---
+++ app/src/main/java/com/example/multiple_media/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.multiple_media;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        MaterialToolbar AppBar = (MaterialToolbar) findViewById(R.id.AppBar);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigationView);
+
+        AppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                drawerLayout.closeDrawer(GravityCompat.START);
+                switch (id){
+                    case R.id.facebook:
+                        replaceFrag(new facebook_frag());break;
+                    case R.id.youtube:
+                        replaceFrag(new youtube_frag());break;
+
+                        default:
+                        return true;
+                }
+                return true;
+            }
+        });
+    }
+    private  void replaceFrag(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+    }
+}
